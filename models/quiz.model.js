15852eda23943cad65fc8470dbe3dc8a0fe78396
hynesnd <hynesnd@gmail.com>
2022-02-09 23:30:00 +0000

    add calculateScore function to quiz model

--- models/quiz.model.js
+++ models/quiz.model.js
@@ -12,3 +12,18 @@ exports.fetchQuestions = () => {
       });
     });
 };
+
+exports.calculateScore = (...answers) => {
+  return fsPromises
+    .readFile(path.resolve(__dirname, "../quizes/quiz.json"), "utf-8")
+    .then((data) => {
+      let quiz = JSON.parse(data);
+      let score = 0;
+      answers.forEach((answer, i) => {
+        if (answer === quiz[i].answer) {
+          score++;
+        }
+      });
+      return score;
+    });
+};
