5eebeeee1c0e8ae01c0739d43656c2f5f9119905
hynesnd <hynesnd@gmail.com>
2022-02-09 19:08:16 +0000

    add GET /api/quiz status 200 test

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,7 +1,7 @@
 const app = require("../app");
 const request = require("supertest");
 
-describe("/api testing: ", () => {
+describe("/api path: ", () => {
   it("Status 200: responds with Hello World", () => {
     return request(app)
       .get("/api")
@@ -9,5 +9,25 @@ describe("/api testing: ", () => {
       .then(({ body }) => {
         expect(body.msg).toBe("Hello World");
       });
+  });
+});
+
+describe("/api/quiz path:", () => {
+  describe("GET method: ", () => {
+    it("Status 200: returns .json containing quiz questions", () => {
+      return request(app)
+        .get("/api/quiz")
+        .expect(200)
+        .then(({ body }) => {
+          expect(Array.isArray(body.quiz)).toBe(true);
+          expect(body.quiz).toHaveLength(3);
+          body.quiz.forEach((qObj) => {
+            expect.objectContaining({
+              question: expect.any(String),
+              options: expect.any(Array),
+            });
+          });
+        });
+    });
   });
 });
