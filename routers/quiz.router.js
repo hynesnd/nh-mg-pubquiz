51c2e1addd9abbaeaf38ee99be98c63f13b71c0a
hynesnd <hynesnd@gmail.com>
2022-02-09 23:31:53 +0000

    add GET /quiz/score route

--- routers/quiz.router.js
+++ routers/quiz.router.js
@@ -1,6 +1,7 @@
 const quizRouter = require("express").Router();
-const { getQuestions } = require("../controllers/quiz.controller");
+const { getQuestions, getScore } = require("../controllers/quiz.controller");
 
 quizRouter.route("/").get(getQuestions);
+quizRouter.route("/score").get(getScore);
 
 module.exports = quizRouter;
