3f746f9c215710b91a36063912728eff1bc62b91
hynesnd <hynesnd@gmail.com>
2022-02-10 14:48:56 +0000

    add 404 handling

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const app = express();
 
 app.use("/api", apiRouter);
 
+//Respond 404: path not found to URLs not beginning with /api
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "path not found" });
+});
+
 module.exports = app;
