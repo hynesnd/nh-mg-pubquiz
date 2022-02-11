cdc6a6e5bba11b1a51f1e3ed88f1970a028fe95a
hynesnd <hynesnd@gmail.com>
2022-02-11 12:56:44 +0000

    add Colors.elm

---
+++ Frontend/src/Colors.elm
@@ -0,0 +1,27 @@
+module Colors exposing (..)
+
+import Element exposing (rgb255)
+
+
+greyBrown =
+    rgb255 68 55 55
+
+
+black =
+    rgb255 39 33 33
+
+
+red =
+    rgb255 255 0 0
+
+
+orange =
+    rgb255 255 77 0
+
+
+green =
+    rgb255 6 255 0
+
+
+white =
+    rgb255 255 255 255
