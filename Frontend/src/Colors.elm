2fb0236122b70ed77829c9207eb70c452a759e5d
hynesnd <hynesnd@gmail.com>
2022-02-12 13:30:11 +0000

    add type annotations

--- Frontend/src/Colors.elm
+++ Frontend/src/Colors.elm
@@ -3,25 +3,31 @@ module Colors exposing (..)
 import Element exposing (rgb255)
 
 
+greyBrown : Element.Color
 greyBrown =
     rgb255 68 55 55
 
 
+black : Element.Color
 black =
     rgb255 39 33 33
 
 
+red : Element.Color
 red =
     rgb255 255 0 0
 
 
+orange : Element.Color
 orange =
     rgb255 255 77 0
 
 
+green : Element.Color
 green =
     rgb255 6 255 0
 
 
+white : Element.Color
 white =
     rgb255 255 255 255
