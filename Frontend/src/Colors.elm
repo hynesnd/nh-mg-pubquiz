cdd150be66b675efde7cbb5da24a669df617da0b
hynesnd <hynesnd@gmail.com>
2022-02-13 16:32:51 +0000

    remove unused colours

--- Frontend/src/Colors.elm
+++ Frontend/src/Colors.elm
@@ -3,31 +3,26 @@ module Colors exposing (..)
 import Element exposing (rgb255)
 
 
-greyBrown : Element.Color
-greyBrown =
-    rgb255 68 55 55
-
-
 black : Element.Color
 black =
     rgb255 39 33 33
 
 
-red : Element.Color
-red =
-    rgb255 255 0 0
-
-
-orange : Element.Color
-orange =
-    rgb255 255 77 0
-
-
-green : Element.Color
-green =
-    rgb255 6 255 0
+shadowBlack : Element.Color
+shadowBlack =
+    rgb255 0 0 0
 
 
 white : Element.Color
 white =
     rgb255 255 255 255
+
+
+zestGreen : Element.Color
+zestGreen =
+    rgb255 44 214 73
+
+
+grey : Element.Color
+grey =
+    rgb255 58 58 62
